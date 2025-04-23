6cb6d17b80537633dffd0dc94ba005444d02c97e
perc-II <skybarrita01@gmail.com>
2025-04-22 21:51:40 -0700

    Update trident.java

--- trident.java
+++ trident.java
@@ -7,27 +7,29 @@
  */
 public class trident extends Weapon
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    int damage;
+    int range;
+    int durability;
+    
+public trident(String name, String status, String description, int damage, int range, int durability){
+    super(name, status, description, damage);
+    this. damage = damage;
+    this.range = range;
+    this.durability = durability;
+}
 
-    /**
-     * Constructor for objects of class trident
-     */
-    public trident()
-    {
-        // initialise instance variables
-        x = 0;
-    }
+public int getDamage(){
+    return damage;
+}
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+public int getRange(){
+    return range;
+}
+
+public int getDurability(){
+    return durability;
+}
+    public void waveCrash(){
+        target.health -= damage;
     }
 }
