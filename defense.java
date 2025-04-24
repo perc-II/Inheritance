0f43af0f461021b325283f56442927310c11c10a
perc-II <skybarrita01@gmail.com>
2025-04-24 10:11:54 -0700

    Update defense.java

--- defense.java
+++ defense.java
@@ -1,1 +1,13 @@
+public abstract class Defense extends Item implements DefenseCommands
+{
+int durability;
 
+public defense(String name, boolean status, String description. int durability){
+super(name, status, description);
+this.durability = durability;
+}
+public defense(){
+super();
+durability = 100;
+}
+}
