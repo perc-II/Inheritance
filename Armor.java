4b2826376c97d90fbfd15d10bcd0d4c5f37814d6
perc-II <skybarrita01@gmail.com>
2025-04-24 10:17:01 -0700

    Update Armor.java

--- Armor.java
+++ Armor.java
@@ -1,1 +1,14 @@
+public class extends Defense
+  {
+    String armor;
 
+public armor(String name, boolean status, String description, int durability){
+  super(name, status, description);
+  this.durability = durability;
+  this.armor = armor;
+}
+public armor(){
+  super();
+  armor = "unicorn";
+}
+    
