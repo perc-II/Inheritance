e52102774356802c0b26026a2803f1c394f7b525
perc-II <skybarrita01@gmail.com>
2025-04-24 13:33:28 -0700

    Update rifle.java

--- rifle.java
+++ rifle.java
@@ -7,27 +7,34 @@
  */
 public class rifle extends Weapon
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    int damage;
+    int range;
+    int durability;
+    int caliber;
 
-    /**
-     * Constructor for objects of class rifle
-     */
-    public rifle()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+public rifle(String name, boolean status, String status, String description, int damage, int range, int durability, int caliber){
+    super(name, status, description, damage);
+    this.damage = damage;
+    this.range = range;
+    this.durability = durability;
+    this.cailber = caliber;
 }
+    public rifle(){
+       super();
+       weapon = "rifle";
+       damage = 60;
+       range = 75;
+       durability = 87;
+       caliber = 10;
+    }
+public boolean poisonedAttack(){
+    int randomNum = (int)Math.random()*10+1);
+    if(randomNum  <= 8){
+    System.out.println("Targets has been poisoned");
+
+    return true;
+}
+return false;
+}
+
+    
